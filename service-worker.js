99706ee6780f27e74193bdea12db36120ee826fc
khalifa3isa <119413684+khalifa3isa@users.noreply.github.com>
2024-07-02 02:31:38 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = 'librasys-cache-v1';
+const assets = [
+  '/librasys/templates/index.html',
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
